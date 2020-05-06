388c19c882af31aa04feb68505b951c7941b6b20
Adam Wolf <adam.wolf.personal@gmail.com>
2020-05-06 16:37:59 +0200

    Add new modules to app component
    And rename back all kotlin foldername to java because of compiling errors

--- app/src/main/java/com/example/mangadex/MainApplicationComponent.kt
+++ app/src/main/java/com/example/mangadex/MainApplicationComponent.kt
@@ -2,13 +2,22 @@ package com.example.mangadex
 
 import com.example.mangadex.interactor.InteractorModule
 import com.example.mangadex.ui.UIModule
+import com.example.mangadex.database.DBModule
+import com.example.mangadex.network.NetworkModule
 import com.example.mangadex.ui.chapter.ChapterActivity
 import com.example.mangadex.ui.main.MainActivity
 import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = [UIModule::class, InteractorModule::class])
+@Component(
+    modules = [
+        NetworkModule::class,
+        DBModule::class,
+        UIModule::class,
+        InteractorModule::class
+    ]
+)
 interface MainApplicationComponent {
     fun inject(chapterActivity: ChapterActivity)
     fun inject(mainActivity: MainActivity)
