88064c143ef4f0931589a47942051ea317822860
Adam Wolf <adam.wolf.personal@gmail.com>
2020-05-16 02:10:06 +0200

    Update network layer + mock
    - Minimum SDK version modified
    - CoRoutines was added (suspend fun)

--- app/src/main/java/com/example/mangadex/network/MangaApi.kt
+++ app/src/main/java/com/example/mangadex/network/MangaApi.kt
@@ -2,20 +2,19 @@ package com.example.mangadex.network
 
 import com.example.mangadex.model.CharacterRequestResult
 import com.example.mangadex.model.MangaRequestResult
-import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface MangaApi {
     @GET("user/{username}/mangalist/all/")
-    fun getMangaList(
+    suspend fun getMangaList(
         @Path("username") username: String,
         @Query("order_by") order_by: String
-    ): Call<MangaRequestResult>
+    ): List<MangaRequestResult>
 
     @GET("manga/{mal_id}/characters")
-    fun getCharacters(
-        @Path("mal_id") mal_id: String
-    ): Call<CharacterRequestResult>
+    suspend fun getCharacters(
+        @Path("mal_id") mal_id: Long
+    ): List<CharacterRequestResult>
 }
